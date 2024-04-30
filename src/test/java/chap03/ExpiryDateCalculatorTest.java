5a45b2f0b437dae46a0ddd07ea9751554d015595
smathj <smathj@naver.com>
2024-05-01 00:40:46 +0900

    두번쨰 테스트 예제 중

---
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,58 @@
+package chap03;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExpiryDateCalculatorTest {
+
+    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
+        assertEquals(expectedExpiryDate, realExpiryDate);
+    }
+    @Test
+    void 만원_납부하면_한달_뒤가_만료일이_됨() {
+        assertExpiryDate(
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(10_000)
+                        .build(),
+                LocalDate.of(2019, 4, 1));
+
+        assertExpiryDate(
+                PayData.builder()
+                    .billingDate(LocalDate.of(2019, 5, 5))
+                    .payAmount(10_000)
+                    .build(),
+                LocalDate.of(2019, 6, 5));
+    }
+
+    @Test
+    void 납부일과_한달_뒤_일자가_같지_않음() {
+        // -- 2019
+        assertExpiryDate(
+                PayData.builder()
+                    .billingDate(LocalDate.of(2019, 1, 31))
+                    .payAmount(10_000)
+                    .build(),
+                LocalDate.of(2019, 2, 28)
+        );
+        assertExpiryDate(
+                PayData.builder()
+                    .billingDate(LocalDate.of(2019, 5, 31))
+                    .payAmount(10_000)
+                    .build(),
+                LocalDate.of(2019, 6, 30)
+        );
+        // -- 2020
+        assertExpiryDate(
+                PayData.builder()
+                    .billingDate(LocalDate.of(2020, 1, 31))
+                    .payAmount(10_000)
+                    .build(),
+                LocalDate.of(2020, 2, 29)
+        );
+    }
+
+}
